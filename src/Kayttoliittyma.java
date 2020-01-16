c952057dac98280a000b874107c08a9f7f4166a3
PihlaParkkinen <pihla.parkkinen@gmail.com>
2020-01-16 16:55:45 +0200

    working app with loop

--- src/Kayttoliittyma.java
+++ src/Kayttoliittyma.java
@@ -11,17 +11,23 @@ public class Kayttoliittyma {
 
     public void kaynnista() {
 
-        System.out.println("Syötä otsikko, kuvaus ja lähde:");
-        String otsikko = lukija.nextLine();
-        String kuvaus = lukija.nextLine();
-        String lahde = lukija.nextLine();
+        while (true) {
+            System.out.println("Syötä otsikko, kuvaus ja lähde:");
+            String otsikko = lukija.nextLine();
+            if (otsikko.equals("Lopeta")) {
+                break;
+            }
+            String kuvaus = lukija.nextLine();
+            String lahde = lukija.nextLine();
 
 
-        Topic t = luoTopic(otsikko, kuvaus, lahde);
+            Topic t = luoTopic(otsikko, kuvaus, lahde);
 
 
-        Tiedostonmuokkaaja kirjoittaja = new Tiedostonmuokkaaja(new File("oppimispaivakirja.txt"));
-        kirjoittaja.kirjoita(t);
+            Tiedostonmuokkaaja kirjoittaja = new Tiedostonmuokkaaja(new File("oppimispaivakirja.txt"));
+            kirjoittaja.kirjoita(t);
+
+        }
     }
 
     public Topic luoTopic(String o, String k, String l) {
