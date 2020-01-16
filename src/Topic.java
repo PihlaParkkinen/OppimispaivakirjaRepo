5997cdbdc9eb3af2e14e268ecc6bf7cdf0b13029
PihlaParkkinen <pihla.parkkinen@gmail.com>
2020-01-16 16:02:16 +0200

    working topic class with basic features

--- src/Topic.java
+++ src/Topic.java
@@ -1,3 +1,43 @@
+import java.time.LocalDate;
+
 public class Topic {
-    
+    private static int id;
+    private final int idcount;
+    private String title;
+    private String description;
+    private String additionalSource;
+    private boolean complete;
+    private LocalDate creationDate;
+    private LocalDate completionDate;
+
+
+    public Topic (String title, String description, String additionalSource) {
+        this.title = title;
+        this.description = description;
+        this.additionalSource = additionalSource;
+        this.complete = false;
+        this.creationDate = LocalDate.now();
+        this.idcount = id++;
+    }
+
+    public void complete() {
+        this.complete = true;
+        this.completionDate = LocalDate.now();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Topic " + this.idcount + " {" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", additionalSource='" + additionalSource + '\'' +
+                ", complete=" + complete +
+                ", creationDate=" + creationDate +
+                ", completionDate=" + completionDate +
+                '}';
+    }
+
+
 }
+
